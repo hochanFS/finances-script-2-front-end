04c10d153158e017e966dbfbdd01067fa19ce44a
Hochan Lee <hochan.lee@financescript.com>
2020-01-12 01:43:23 -0500

    added the navigation design for phone device #6

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+// const navSlide = () => {
+//     const phone_navigation = document.querySelector('.navigation__pwrapper')
+//     const nav = document.querySelector('.navigation__phone--active');
+
+//     phone_navigation.addEventListener('click', () => {
+//         nav.classList.toggle('navigation__phone--active');
+//     })
+// }
+
+// navSlide();
